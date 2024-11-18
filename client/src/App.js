e25d12cc815ab2b695c56f76d7513061abc2d1eb
Swetha Prakash P <swetha.ppk.4@gmail.com>
2024-11-18 18:02:10 +0400

    inital setup frontend

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <div>
+      <h1>Expense Tracker</h1>
+      <Button variant="contained" color="primary">
+        Start Tracking Expenses
+      </Button>
+    </div>
+  );
+}
+
+export default App;
